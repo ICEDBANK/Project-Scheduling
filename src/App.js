2740b62e623bd759fa7daa6f3f1721f1c97712e8
ICEDBANK <j.e.rice4101@gmail.com>
2024-05-17 16:05:51 -0400

    Navbar Creation

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-// App.js
+// src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
-import About from './components/About';
 import Schedule from './components/Schedule';
+import About from './components/About';
+import CustomNavbar from './components/Navbar';
 
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact component={Dashboard} />
-        <Route path="/schedule" component={Schedule} />
-        <Route path="/about" component={About} />
-      </Switch>
+      <CustomNavbar />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/schedule" element={<Schedule />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </Router>
   );
 };
--- src/components/CustomNavbar.js
+++ src/components/CustomNavbar.js
@@ -1,14 +1,21 @@
-// CustomNavbar.js
+// src/components/Navbar.js
 import React from 'react';
-import Link
+import { NavLink } from 'react-router-dom';
+import { Navbar, Nav } from 'react-bootstrap';
 
 const CustomNavbar = () => {
   return (
-    <>
-    
-
-
-    </>
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand as={NavLink} to="/">Scheduling App</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link as={NavLink} to="/" exact>Dashboard</Nav.Link>
+          <Nav.Link as={NavLink} to="/schedule">Schedule</Nav.Link>
+          <Nav.Link as={NavLink} to="/about">About</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 };
 
