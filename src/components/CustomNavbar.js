640c1107b0940fa379c9759e014d616ee6a99a52
ICEDBANK <j.e.rice4101@gmail.com>
2024-05-20 14:39:57 -0400

    Automated commit

--- src/components/CustomNavbar.js
+++ src/components/CustomNavbar.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
-import { LinkContainer } from 'react-router-bootstrap';
 import { NavLink } from 'react-router-dom';
 
 const CustomNavbar = ({ to = '/', children = 'Link' }) => {
@@ -10,21 +9,21 @@ const CustomNavbar = ({ to = '/', children = 'Link' }) => {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
-          <LinkContainer to="/">
+
             <Nav.Link as={NavLink} to="/">Dashboard</Nav.Link>
-          </LinkContainer>
-          <LinkContainer to="/schedule">
+    
+   
             <Nav.Link as={NavLink} to="/schedule">Schedule Form</Nav.Link>
-          </LinkContainer>
-          <LinkContainer to="/machine-lt7">
+    
+       
             <Nav.Link as={NavLink} to="/machine-lt7">Machine LT7</Nav.Link>
-          </LinkContainer>
-          <LinkContainer to="/machine-lt8">
+      
+          
             <Nav.Link as={NavLink} to="/machine-lt8">Machine LT8</Nav.Link>
-          </LinkContainer>
-          <LinkContainer to="/machine-lt712">
+          
+          
             <Nav.Link as={NavLink} to="/machine-lt712">Machine LT712</Nav.Link>
-          </LinkContainer>
+          
         </Nav>
       </Navbar.Collapse>
     </Navbar>
